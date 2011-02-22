fec7645ed8b46415e4d7482d381dbf1b95d108b1
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2011-02-22 10:33:08 +0000

    Added support for complete & dissimilarity archiving.

--- optimisation/archive_add.m
+++ optimisation/archive_add.m
@@ -1,6 +1,6 @@
 function ark = archive_add(ark, args, objs)
 % archive_add(ark, args, objs)
-% Add data to an archive or list of archives.
+% Add data to an archive or cell array of archives.
 % - objs is a column vector of objective function values
 %   (this function assumes we are maximising)
 % - each row of args should be the the parameters achieving the
@@ -8,7 +8,7 @@ function ark = archive_add(ark, args, objs)
 
     if (length(ark) ~= 1)
         for i = 1:length(ark)
-            ark(i) = archive_add(ark(i), data);
+            ark{i} = archive_add(ark{i}, args, objs);
         end
         return;
     end
@@ -23,13 +23,64 @@ function ark = archive_add(ark, args, objs)
         error('differing numbers of arguments in archive and data');
     end
 
-    if (ark.type == 'best')
+    if strcmp(ark.type, 'best')
         unsorted_args = [ark.args; args];
         [sorted_objs, ix] = sort([ark.objs; objs], 'descend');
         
-        ark.objs = sorted_objs(1:ark.size);
-        ark.args = unsorted_args(ix(1:ark.size), :);
+        new_size = min(ark.size, size(unsorted_args, 1));
+        
+        ark.objs = sorted_objs(1:new_size);
+        ark.args = unsorted_args(ix(1:new_size), :);
+    elseif strcmp(ark.type, 'complete')
+        ark.objs = [ark.objs; objs];
+        ark.args = [ark.args; args];
+    elseif strcmp(ark.type, 'dissimilarity')
+        
+        for i = 1:length(objs)
+            % Dissimilarity archiving as given in lecture notes.
+            if (length(ark.objs) < ark.size)
+                % archive not full: archive if dissimilar to entries so far
+                if (all(row_dist(ark.args, args(i,:)) > ark.dmin))
+                    ark.args = [ark.args; args(i,:)];
+                    ark.objs = [ark.objs; objs(i)];
+                end
+            elseif (all(row_dist(ark.args, args(i,:)) > ark.dmin))
+                % archive full, new entry dissimilar to all prev. entries
+                % archive if better than worst
+                [obj_worst, i_worst] = min(ark.objs);
+                if (objs(i) > obj_worst)
+                    ark.args(i_worst,:) = args(i,:);
+                    ark.objs(i_worst) = objs(i);
+                end
+            elseif (all(ark.objs < objs(i)))
+                % best so far: archive, replacing closest
+                [~, i_closest] = min(row_dist(ark.args, args(i,:)));
+                ark.args(i_closest,:) = args(i,:);
+                ark.objs(i_closest) = objs(i);
+            else
+                % see if v. similar to a solution, and better than it
+                similar = row_dist(ark.args, args(i,:)) < ark.dsim;
+                can_replace = similar & (ark.objs < objs(i));
+                if any(can_replace)
+                    i_replace = find(can_replace, 1);
+                    ark.args(i_replace,:) = args(i,:);
+                    ark.objs(i_replace) = objs(i);  
+                end
+            end
+        end
+        
     else
         error(['Unknown archive type: ' ark.type]);
     end
+end
+
+function ans = row_dist(a, b)
+    if ~isempty(a)
+        ans = sqrt(sum(bsxfun(@minus, a, b) .^ 2, 2));
+    else
+        ans = [];
+    end
+    
+end
+
     
--- optimisation/archive_new.m
+++ optimisation/archive_new.m
@@ -1,7 +1,12 @@
-function ark = archive_new(size, type)
-% archive_new(size, type)
+function ark = archive_new(type, size, varargin)
+% archive_new(type, size)
 % Create a new archive struct of the given size and type.
-    ark.size = size;
     ark.type = type;
+    ark.size = size;
     ark.objs = [];
     ark.args = [];
+    
+    if strcmp(type, 'dissimilarity')
+        ark.dmin = varargin{1};
+        ark.dsim = varargin{2};
+    end
