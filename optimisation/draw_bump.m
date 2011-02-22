b7ad6b185f276931869153cf70ef3e31821e91f0
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2011-02-22 10:42:47 +0000

    Functions to visually evaluate performance of a search.

---
+++ optimisation/draw_bump.m
@@ -0,0 +1,12 @@
+function draw_bump(granularity)
+    if nargin < 1
+        granularity = 0.03;
+    end
+    
+    [x,y] = meshgrid(0:granularity:10, 0:granularity:10);
+    b = bump(x,y);
+    b(bump_penalty(x,y) ~= 0) = NaN;
+    
+    h = surf(x,y,b);
+    set(h, 'edgecolor', 'none');
+    view(176,36);
