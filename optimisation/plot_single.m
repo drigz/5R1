0464434a24fca93a70ffe2c418186b10b426a998
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2011-03-10 21:21:42 +0000

    Added options to plot_single

--- optimisation/plot_single.m
+++ optimisation/plot_single.m
@@ -1,4 +1,4 @@
-function plot_single(results, variables, n_iters, i)
+function plot_single(results, variables, n_iters, i, 'varargin')
 
     values = [];
     means = [];
@@ -8,14 +8,25 @@ function plot_single(results, variables, n_iters, i)
         valid = results(arrayfun(@(r) r.args{i} == value, results));
         [~, best] = max(arrayfun(@(r) r.mean, valid));
 
+        if strcmp('show', varargin)
+            valid(best)
+        end
+
         values = [values value];
         means = [means valid(best).mean];
         stds = [stds valid(best).std];
     end
 
-    errorbar(values, means, 2*stds, 'r');
-    errorbarlogx
-    hold on
-    errorbar(values, means, 2*stds/sqrt(n_iters), 'b');
-    errorbarlogx
+    if strcmp('stds', varargin)
+        errorbar(values, means, 2*stds, 'xr');
+        if strcmp('logx', varargin)
+            errorbarlogx
+        end
+        hold on
+    end
+
+    errorbar(values, means, 2*stds/sqrt(n_iters), 'xb');
+    if strcmp('logx', varargin)
+        errorbarlogx
+    end
     hold off
