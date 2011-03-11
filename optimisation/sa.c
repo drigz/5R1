d1be092d3f42ea18655c69242e4662a19150afbf
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2011-03-11 16:20:33 +0000

    Bringing sa.c in line with sa.m
    double-precision arithmetic
    faster gaussian samples
    fixed differences in huang implementation

--- optimisation/sa.c
+++ optimisation/sa.c
@@ -5,42 +5,74 @@
 #include <unistd.h>
 #include <string.h>
 
+#if 0
+    typedef float real;
+#define powr powf
+#define logr logf
+#define cosr cosf
+#define sinr sinf
+#define fabsr fabsf
+#define sqrtr sqrtf
+#else
+    typedef double real;
+#define powr pow
+#define logr log
+#define cosr cos
+#define sinr sin
+#define fabsr fabs
+#define sqrtr sqrt
+#endif
+
 enum {
     uniform,
     gaussian,
     parks,
 } step_method;
 
-float init_step_size;
-float penalty_weight;
+real init_step_size;
+real penalty_weight;
 enum {
     kirkpatrick,
     white,
     constant,
 } initial_temp_method;
-float initial_temp;
+real initial_temp;
 int temp_length;
 enum {
     huang,
     exponential,
 } temp_decay_method;
-float temp_decay;
+real temp_decay;
 
-float randf()
+real randf()
 {
-    return random() / powf(2, 31) + 1 / powf(2, 32);
+    return random() / powr(2, 31) + 1 / powr(2, 32);
 }
 
-float randn()
+real randn()
 {
-    float u1 = randf(), u2 = randf();
+    static bool prepped = false;
+    static real prepped_result = 0;
 
-    return sqrtf(-2 * logf(u1)) * cosf(2*M_PI*u2);
+    if (prepped)
+    {
+        prepped = false;
+        return prepped_result;
+    }
+    else
+    {
+        real u1 = randf(), u2 = randf();
+        real len = sqrtr(-2 * logr(u1));
+        prepped = true;
+        prepped_result = len * sinr(2*M_PI*u2);
+
+        return len * cosr(2*M_PI*u2);
+    }
 }
 
-float mean(float x[], int n)
+real mean(real x[], int n)
 {
-    float sx = 0;
+    real sx = 0;
 
     for (int i=0; i<n; i++)
     {
@@ -50,9 +82,9 @@ float mean(float x[], int n)
     return sx / n;
 }
 
-float std(float x[], int n)
+real std(real x[], int n)
 {
-    float sx = 0, sxx = 0;
+    real sx = 0, sxx = 0;
 
     for (int i=0; i<n; i++)
     {
@@ -63,24 +95,24 @@ float std(float x[], int n)
     return sqrt((sxx - sx*sx/n) / (n-1));
 }
 
-float bump(float x, float y)
+real bump(real x, real y)
 {
-    float cxs = cosf(x); cxs *= cxs;
-    float cys = cosf(y); cys *= cys;
+    real cxs = cosr(x); cxs *= cxs;
+    real cys = cosr(y); cys *= cys;
 
-    return fabsf((cxs*cxs + cys*cys - 2*cxs*cys) / sqrtf(x*x+2*y*y));
+    return fabsr((cxs*cxs + cys*cys - 2*cxs*cys) / sqrtr(x*x+2*y*y));
 }
 
-float penalty(float x, float y)
+real penalty(real x, real y)
 {
 #define BOUND(x) ((x) < 0 ? x : 0)
     return BOUND(x-0)+BOUND(10-x)+BOUND(y-0)+BOUND(10-y)+BOUND(15-x-y)+BOUND(x*y-0.75);
 }
 
-float T_kirkpatrick(float x[], int n)
+real T_kirkpatrick(real x[], int n)
 {
     int n_neg = 0;
-    float sx_neg = 0;
+    real sx_neg = 0;
 
     for (int i=0; i<n; i++)
     {
@@ -91,29 +123,29 @@ float T_kirkpatrick(float x[], int n)
         }
     }
 
-    return - (sx_neg / n_neg) / logf(0.8);
+    return - (sx_neg / n_neg) / logr(0.8);
 }
 
-float sa(unsigned seed)
+real sa(unsigned seed)
 {
     srandom(seed);
 
-    float T;
+    real T;
     if (initial_temp_method != constant)
         T = INFINITY;
     else
         T = initial_temp;
 
-    float step_size_x = init_step_size, step_size_y = init_step_size;
-    float pos_x = 5, pos_y = 5;
-    float obj = bump(pos_x, pos_y);
-    float obj_pen = obj;
+    real step_size_x = init_step_size, step_size_y = init_step_size;
+    real pos_x = 5, pos_y = 5;
+    real obj = bump(pos_x, pos_y);
+    real obj_pen = obj;
 
     int samples_remaining = 5000-1;
 
-    float obj_d[5000];
+    real obj_d[5000];
     int n_obj_d = 0;
-    float accepts[5000];
+    real accepts[5000];
     int n_accepts = 0;
 
     int num_trials = 0, num_acceptances = 0;
@@ -121,10 +153,10 @@ float sa(unsigned seed)
     int max_trials = temp_length;
     int max_acceptances = 0.6*temp_length;
 
-    float alpha = 0.1, omega = 2.1;
+    real alpha = 0.1, omega = 2.1;
 
-    float best_obj = obj;
-    float best_x = pos_x, best_y = pos_y;
+    real best_obj = obj;
+    real best_x = pos_x, best_y = pos_y;
     int best_time = samples_remaining;
 
     while (samples_remaining > 0)
@@ -140,7 +172,7 @@ float sa(unsigned seed)
             step_size_x = step_size_y = init_step_size;
         }
 
-        float step_x, step_y;
+        real step_x, step_y;
         if (step_method == gaussian)
         {
             step_x = step_size_x * randn();
@@ -152,15 +184,15 @@ float sa(unsigned seed)
             step_y = step_size_y * (2*randf()-1);
         }
 
-        float new_x = pos_x+step_x, new_y = pos_y+step_y;
+        real new_x = pos_x+step_x, new_y = pos_y+step_y;
 
-        float new_pen = penalty_weight * penalty(new_x, new_y);
+        real new_pen = penalty_weight * penalty(new_x, new_y);
 
         if (T == INFINITY && new_pen != 0)
             continue;
 
-        float new_obj = bump(new_x, new_y);
-        float new_obj_pen = new_obj + new_pen / T;
+        real new_obj = bump(new_x, new_y);
+        real new_obj_pen = new_obj + new_pen / T;
         samples_remaining--;
 
         num_trials++;
@@ -176,10 +208,10 @@ float sa(unsigned seed)
             }
         }
 
-        float p;
+        real p;
         if (step_method == parks)
         {
-            float step_norm = sqrtf(step_x*step_x + step_y*step_y);
+            real step_norm = sqrtr(step_x*step_x + step_y*step_y);
             p = exp(- (obj_pen - new_obj_pen) / (T * step_norm));
         }
         else
@@ -200,8 +232,8 @@ float sa(unsigned seed)
 
             if (T != INFINITY && step_method == parks)
             {
-                step_size_x = (1-alpha)*step_size_x + alpha*omega*fabsf(step_x);
-                step_size_y = (1-alpha)*step_size_y + alpha*omega*fabsf(step_y);
+                step_size_x = (1-alpha)*step_size_x + alpha*omega*fabsr(step_x);
+                step_size_y = (1-alpha)*step_size_y + alpha*omega*fabsr(step_y);
             }
         }
 
@@ -227,9 +259,9 @@ float sa(unsigned seed)
         {
             if (temp_decay_method == huang)
             {
-                float factor;
+                real factor;
                 if (n_accepts < 2)
-                    factor = 0.9999;
+                    factor = 0.5;
                 else
                 {
                     factor = exp(-0.7*T/std(accepts, n_accepts));
@@ -249,7 +281,8 @@ float sa(unsigned seed)
             n_obj_d = 0;
             num_trials = 0;
             num_acceptances = 0;
-            n_accepts = 0;
+            n_accepts = 1;
+            accepts[0] = obj_pen;
         }
     }
 
@@ -301,15 +334,15 @@ int main(int argc, char **argv)
         temp_decay = atof(argv[7]);
     }
 
-    float results[n_iters];
+    real results[n_iters];
 
     for (int i=0; i<n_iters; i++)
     {
-        results[i] = sa(getpid() * n_iters + i);
+        results[i] = sa(i);
     }
 
-    float m = mean(results, n_iters);
-    float s = std(results, n_iters);
+    real m = mean(results, n_iters);
+    real s = std(results, n_iters);
 
     printf("N(%g, %g)\n", m, s);
     printf("[%g, %g]\n", m-2*s/sqrt(n_iters), m+2*s/sqrt(n_iters));
