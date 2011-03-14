9985625858eed31000b66c7f0d1c9fc61cdcb763
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2011-03-14 11:36:37 +0000

    scatter_by changed to work for SA results

--- optimisation/scatter_by.m
+++ optimisation/scatter_by.m
@@ -8,12 +8,24 @@ function scatter_by(results, variables, i, varargin)
     set(gca, 'ColorOrder', colours);
 
     for j = 1:length(variables{i})
-        value = variables{i}(j);
-        valid = results(arrayfun(@(r) r.args{i} == value, results));
+        if iscell(variables{i})
+            value = variables{i}{j};
+        else
+            value = variables{i}(j);
+        end
+        if ischar(value)
+            valid = results(arrayfun(@(r) strcmp(r.args{i}, value), results));
+        else
+            valid = results(arrayfun(@(r) r.args{i} == value, results));
+        end
         scatter([valid.mean], [valid.std], 4, markers(j));
         xlabel('mean \mu');
         ylabel('standard deviation \sigma');
         grid on
     end
 
-    legend(arrayfun(@num2str, variables{i}, 'UniformOutput', false))
+    if iscell(variables{i})
+        legend(variables{i})
+    else
+        legend(arrayfun(@num2str, variables{i}, 'UniformOutput', false))
+    end
