ad1ed5d5372a0bf3faca9fd39a7681b2935ae755
Rodrigo Queiro <rodrigo.queiro@gmail.com>
2011-03-15 12:13:18 +0000

    Use random sampling in scatter_by

--- optimisation/scatter_by.m
+++ optimisation/scatter_by.m
@@ -5,23 +5,26 @@ function scatter_by(results, variables, i, varargin)
     colours = varycolor(length(variables{i}));
     markers = '+o*xsd^vph<>';
 
-    set(gca, 'ColorOrder', colours);
+    % set(gca, 'ColorOrder', colours);
 
-    for j = 1:length(variables{i})
-        if iscell(variables{i})
-            value = variables{i}{j};
-        else
-            value = variables{i}(j);
+    for k = 1:10
+        subresults = results(randsample(length(results), 100));
+        for j = 1:length(variables{i})
+            if iscell(variables{i})
+                value = variables{i}{j};
+            else
+                value = variables{i}(j);
+            end
+            if ischar(value)
+                valid = subresults(arrayfun(@(r) strcmp(r.args{i}, value), subresults));
+            else
+                valid = subresults(arrayfun(@(r) r.args{i} == value, subresults));
+            end
+            scatter([valid.mean], [valid.std], 4, colours(j, :), markers(j));
+            xlabel('mean \mu');
+            ylabel('standard deviation \sigma');
+            grid on
         end
-        if ischar(value)
-            valid = results(arrayfun(@(r) strcmp(r.args{i}, value), results));
-        else
-            valid = results(arrayfun(@(r) r.args{i} == value, results));
-        end
-        scatter([valid.mean], [valid.std], 4, markers(j));
-        xlabel('mean \mu');
-        ylabel('standard deviation \sigma');
-        grid on
     end
 
     if iscell(variables{i})
